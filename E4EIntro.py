b2cfee761bddd20a52a5f75cf78722267ef49fe8
pranjalc1 <86537904+pranjalc1@users.noreply.github.com>
2022-06-24 13:07:09 -0700

    finished intro assignment

---
+++ E4EIntro.py
@@ -0,0 +1,70 @@
+import pandas as pd
+
+
+def stratified_random_sample(csv_path):
+    """Creates a stratified random sample of a csv file
+
+    Args:
+        csv_path (str): path to csv file with complete data
+
+    Returns:
+        bool: whether the sampling is successful or not
+    """
+    # reads csv file
+    data = pd.read_csv(csv_path, low_memory=False)
+
+    # filter out data that is less than one minute and/or has an na StartDateTime
+    data = data[data["Duration"] > 60]
+    data = data.dropna(axis="rows", subset=["StartDateTime"])
+
+    # create data frame
+    stratified_random_sample = pd.DataFrame(columns=data.columns)
+
+    # iterate through all audiomoths
+    for audiomoth in data.AudioMothCode.unique():
+
+        # variable to check if it has all hours
+        hasAllHours = True
+
+        # create filtered data
+        specific_data = data[data.AudioMothCode == audiomoth]
+
+        # create data that will hold sample
+        audiomoth_sample = pd.DataFrame(columns=data.columns)
+
+        # iterate through all hours
+        for hour in range(24):
+
+            # create string to search for in StartDateTime column
+            if (hour < 10):
+                time_string = " 0" + str(hour)
+            else:
+                time_string = " " + str(hour)
+
+            # filter hour-specific data
+            hourly_data = specific_data[specific_data["StartDateTime"].str.contains(
+                time_string)]
+
+            # only continue if there exists a data entry for this hour
+            if len(hourly_data) == 0:
+                hasAllHours = False
+                break
+            audiomoth_sample = pd.concat(
+                [audiomoth_sample, hourly_data.sample(1)])
+
+        # only add audiomoth_sample if has sample from each hour
+        if not hasAllHours:
+            hasAllHours = True
+            continue
+        stratified_random_sample = pd.concat(
+            [stratified_random_sample, audiomoth_sample])
+
+    # convert DataFrame to csv file
+    stratified_random_sample.to_csv(
+        csv_path[:-4] + "_stratified_random_sample.csv", index=False)
+
+    # returns True if completed
+    return True
+
+
+print(stratified_random_sample("Peru_2019_AudioMoth_Data_Full.csv"))
